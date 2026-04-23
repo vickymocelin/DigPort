18bea268731c0f3babf6041eadf61640220d1953
vickymocelin <vickymocelin@gmail.com>
2026-04-23 14:40:08 -0300

    initial commit

---
+++ dadosUsuario.js
@@ -0,0 +1,6 @@
+let nome = "Maria";
+let idade = 30;
+const anoDeNascimento = 1980;
+const ano = 2028;
+let somaFuturo = ano - anoDeNascimento;
+console.log(`A idade de ${nome} em 2028 será ${somaFuturo}. `);
---
+++ exercicioVariaveis.js
@@ -0,0 +1,7 @@
+let nome = "Victoria";
+let idade = 15;
+let signo = "Peixes";
+let laveiCabelo = true;
+let defeitos = null;
+
+console.log("Nome: ", nome,"\nIdade: ", idade);
